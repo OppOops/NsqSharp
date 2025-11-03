17a07be19b4ae5e923102533ef1e709902d4bbb1
YKH-dabit <YKH.dabit@gmail.com>
2025-11-03 17:53:51 +0800

    port to .net6/.net8

---
+++ NsqSharp.Core.Tests/Utils/Extensions/RNGCryptoServiceProviderExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using NsqSharp.Utils.Extensions;
+using NUnit.Framework;
+
+namespace NsqSharp.Tests.Utils.Extensions
+{
+    [TestFixture]
+    public class RNGCryptoServiceProviderExtensionsTest
+    {
+        [Test]
+        public void Float64()
+        {
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                double value = Random.Shared.NextDouble();
+                Assert.GreaterOrEqual(value, 0);
+                Assert.Less(value, 1);
+            }
+        }
+    }
+}
